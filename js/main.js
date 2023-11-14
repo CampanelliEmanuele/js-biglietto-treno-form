0ea2f0a3fd4add4400861326436f1ef9fa0e29fd
CampanelliEmanuele <emanuele.campanelli2001@gmail.com>
2023-11-14 16:16:16 +0100

    Add: basic js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+const submitBtn = document.getElementById("submitBtn");
+
+submitBtn.addEventListener("click", function () { 
+    event.preventDefault();
+    
+    let name = document.getElementById("inputName").value;
+    let surname = document.getElementById("inputSurname").value;
+    let km = document.getElementById("inputKm").value;
+    let age = document.getElementById("inputAge").value;
+
+
+});
