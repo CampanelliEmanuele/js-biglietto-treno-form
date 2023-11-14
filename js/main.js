d59a1bb0d745ea336b58074191509aed2b2df11c
CampanelliEmanuele <emanuele.campanelli2001@gmail.com>
2023-11-14 18:37:05 +0100

    Exercise completed

--- js/main.js
+++ js/main.js
@@ -8,13 +8,32 @@ submitBtn.addEventListener("click", function () {
     let name = document.getElementById("inputName").value;
     let surname = document.getElementById("inputSurname").value;
     let km = document.getElementById("inputKm").value;
-    let age = document.getElementById("inputAge").value;
+    let ageCase = document.getElementById("inputAge").value;
 
     document.getElementById("ticket").classList.remove("d-none");
 
-    document.getElementById("ticketCompleteName").append(name)
-    document.getElementById("ticketType").append(surname)
-    document.getElementById("ticketCode").append(km)
-    document.getElementById("ticketPrice").append(age)
+    let ticketCode = Math.floor(Math.random() * 10001);
+
+    let basePrice = 0.21 * km;
+    let onePercent = basePrice / 100;
+    let finalPrice = basePrice;
+
+    switch (ageCase) {
+        case "middleAge":
+            finalPrice = basePrice - 20 * onePercent;
+            break;
+        case "overAge":
+            finalPrice = basePrice - 40 * onePercent;
+            break;
+    }
+    let ticketPrice = finalPrice.toFixed(2);
+
+    document.getElementById("ticketCompleteName").innerHTML = name
+    document.getElementById("ticketType").innerHTML = surname
+    document.getElementById("ticketDistance").innerHTML = km
+    document.getElementById("ticketCode").innerHTML = ticketCode
+    document.getElementById("ticketPrice").innerHTML = ticketPrice + '$'
 
 });
+
+
