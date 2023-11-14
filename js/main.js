9e5e77eecbdf5919e5463056a32bc61160bd4dea
CampanelliEmanuele <emanuele.campanelli2001@gmail.com>
2023-11-14 17:38:30 +0100

    ticket infos are now displayed

--- js/main.js
+++ js/main.js
@@ -2,14 +2,19 @@
 
 const submitBtn = document.getElementById("submitBtn");
 
-submitBtn.addEventListener("click", function () { 
+submitBtn.addEventListener("click", function () {
     event.preventDefault();
-    
+
     let name = document.getElementById("inputName").value;
     let surname = document.getElementById("inputSurname").value;
     let km = document.getElementById("inputKm").value;
     let age = document.getElementById("inputAge").value;
 
     document.getElementById("ticket").classList.remove("d-none");
+
+    document.getElementById("ticketCompleteName").append(name)
+    document.getElementById("ticketType").append(surname)
+    document.getElementById("ticketCode").append(km)
+    document.getElementById("ticketPrice").append(age)
 
 });
