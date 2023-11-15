ff2170a23261511114a3cc3914002f2b6eef174d
CampanelliEmanuele <emanuele.campanelli2001@gmail.com>
2023-11-15 09:59:22 +0100

    html&js code improvements

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 const submitBtn = document.getElementById("submitBtn");
 
-submitBtn.addEventListener("click", function () {
+submitBtn.addEventListener("click", function(event) {
     event.preventDefault();
 
     let name = document.getElementById("inputName").value;
@@ -13,23 +13,21 @@ submitBtn.addEventListener("click", function () {
     document.getElementById("ticket").classList.remove("d-none");
 
     let ticketCode = Math.floor(Math.random() * 10001);
-
     let basePrice = 0.21 * km;
-    let onePercent = basePrice / 100;
     let finalPrice = basePrice;
 
     switch (ageCase) {
         case "middleAge":
-            finalPrice = basePrice - 20 * onePercent;
+            finalPrice = basePrice * 0.8;
             break;
         case "overAge":
-            finalPrice = basePrice - 40 * onePercent;
+            finalPrice = basePrice * 0.6;
             break;
     }
     let ticketPrice = finalPrice.toFixed(2);
 
     document.getElementById("ticketCompleteName").innerHTML = name + ' ' + surname
-    document.getElementById("ticketDistance").innerHTML = km
+    document.getElementById("ticketDistance").innerHTML = km + ' km'
     document.getElementById("ticketCode").innerHTML = ticketCode
     document.getElementById("ticketPrice").innerHTML = ticketPrice + '$'
 
